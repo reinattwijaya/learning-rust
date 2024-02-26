9caf11789df584eeecef8df320d688d22b313362
reinattwijaya <71814354+reinattwijaya@users.noreply.github.com>
2024-02-26 17:17:14 +0900

    first folders

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/reinattwijaya/Documents/github/learning-rust/guessing_game/target/debug/guessing_game: /Users/reinattwijaya/Documents/github/learning-rust/guessing_game/src/main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/reinattwijaya/Documents/github/learning-rust/hello_cargo/target/debug/hello_cargo: /Users/reinattwijaya/Documents/github/learning-rust/hello_cargo/src/main.rs
